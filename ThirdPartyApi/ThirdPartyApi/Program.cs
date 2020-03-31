6b7d70569ebbcb8d0717d75850359d856c1f3050
purkayasta <pritompurkayasta@gmail.com>
2020-04-01 04:15:24 +0600

    Fetch From third party Api

---
+++ ThirdPartyApi/ThirdPartyApi/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace ThirdPartyApi
+{
+    class Program
+    {
+        private const string ApiUrl = "https://sv443.net/jokeapi/v2/joke/Programming?type=single";
+
+        static void Main(string[] args)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var responseFromApi = httpClient.GetAsync(ApiUrl).Result;
+                if (responseFromApi.IsSuccessStatusCode)
+                {
+                    var responseAsString = responseFromApi.Content.ReadAsStringAsync();
+
+                    var jokes = (JokesApi) JsonConvert.DeserializeObject(responseAsString.Result, typeof(JokesApi));
+
+                    if (jokes != null)
+                    {
+                        Console.WriteLine(jokes.Joke);
+                    }                        
+                }
+            }
+        }
+    }
+
+    internal class JokesApi
+    {
+        public string Id { get; set; }
+        public string Category { get; set; }
+        public string Type { get; set; }
+        public string Joke { get; set; }
+        public string Error { get; set; }
+    }
+
+}
---
+++ ThirdPartyApi/readme.md
@@ -0,0 +1,2 @@
+# How to Consume Any Web Api Response in C# 🎈🎃🎨
+Link to medium Article => https://medium.com/@pritompurkayasta/third-party-api-to-c-a-wonderful-journey-to-the-rabbit-hole-39d2cd79d107
